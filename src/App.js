64c3e00ec0bc294ba424242bbfd0625d4e2f9b88
AbenaAmo13 <60615703+AbenaAmo13@users.noreply.github.com>
2021-10-23 15:22:35 +0100

    First deployment--> Ben sucess

--- src/App.js
+++ src/App.js
@@ -1,59 +1,39 @@
-import React from "react";
-import coverpicture from './coverpage.jpeg'
-import HTMLFlipBook from 'react-pageflip';
+import logo from './logo.svg';
 import './App.css';
-import {Paper,Button, CardActionArea, CardActions, CardContent, CardMedia, Typography, Card} from "@mui/material";
-const Page = React.forwardRef((props, ref) => {
-    return (
-        <div className="demoPage" ref={ref}> /* ref required */
-            <h1>Page Header</h1>
-            <p>{props.children}</p>
-            <p>Page number: {props.number}</p>
-        </div>
-    );
-});
-function MyBook(props) {
-    return (
-        <div>
+import {Component} from "react";
+import MainMenu from "./MainMenu";
 
 
-        <HTMLFlipBook width={300} height={500} className="App">
+export default class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick = (newPage) => {
+        this.setState({currentPage: newPage});
+    }
 
-            <Paper className="demoPage">
-                <Card>
-                    <CardActionArea>
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            image="coverpicture"
-                            alt="green iguana"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                HAPPY BIRTHDAY MUMMY!!!
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                This web book was created by me to show you how much I love you. There are million reasons why I love you but let me start with 10
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                        <Button size="small" color="primary">
-                            Share
-                        </Button>
-                    </CardActions>
-            </Card>
+    getCurrentPage(){
+        return this.state.currentPage
+    }
+
+    render() {
+
+        if (this.getCurrentPage() === 0){
+            return(
+                <MainMenu currentPage={this.state.currentPage} handleClick={this.handleClick}>
+
+                </MainMenu>
+            )
+        }
+        else if(this.getCurrentPage() === 1){
+            return (
+                <div>Hey</div>
+            )
+        }}
 
 
-            </Paper>
-
-
-            <Paper className="demoPage">Page 2</Paper>
-            <div className="demoPage">Page 3</div>
-            <div className="demoPage">Page 4</div>
-        </HTMLFlipBook>
-        </div>
-    );
 }
-
-export default MyBook;
---
+++ src/MyBook.js
@@ -0,0 +1,61 @@
+import HTMLFlipBook from 'react-pageflip';
+import React from "react";
+import coverpicture from './coverpage.jpeg'
+import {Paper,Button, CardActionArea, CardActions, CardContent, CardMedia, Typography, Card} from "@mui/material";
+
+const Page = React.forwardRef((props, ref) => {
+    return (
+        <div className="demoPage" ref={ref}> /* ref required */
+            <h1>Page Header</h1>
+            <p>{props.children}</p>
+            <p>Page number: {props.number}</p>
+        </div>
+    );
+});
+
+function MyBook(props) {
+    return (
+        <div>
+
+            <HTMLFlipBook width={300} height={500} className="App">
+
+                <Paper className="demoPage">
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image="coverpicture"
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    HAPPY BIRTHDAY MUMMY!!!
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    This web book was created by me to show you how much I love you. There are million reasons why I love you but let me start with 10
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                        </CardActions>
+                    </Card>
+
+
+                </Paper>
+
+
+                <Paper className="demoPage">Dora the explorer</Paper>
+                <div className="demoPage">Harry styles</div>
+                <div className="demoPage">BTS</div>
+            </HTMLFlipBook>
+        </div>
+    );
+}
+
+
+
+export default MyBook;
